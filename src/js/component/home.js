15adfa1aa9431d6b195dec58d5f3cd4a9d1a6d22
Carlos Molina <carlos170373@gmail.com>
2019-10-05 15:35:48 +0000

    primera parte todo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,178 +2,75 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap";
 
-let menu = [
-	{
-		label: "About",
-		url:
-			"https://blackrockdigital.github.io/startbootstrap-heroic-features/#"
-	},
-	{
-		label: "Service",
-		url:
-			"https://blackrockdigital.github.io/startbootstrap-heroic-features/#"
-	},
-	{
-		label: "Contact",
-		url:
-			"https://blackrockdigital.github.io/startbootstrap-heroic-features/#"
+export class ControlledImput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: "",
+			data: []
+		};
 	}
-];
 
-let menuStar = [
-	{
-		label: "Start Bootstrap",
-		url:
-			"https://blackrockdigital.github.io/startbootstrap-heroic-features/#"
-	}
-];
+	Change = e => {
+		this.setState({ value: e.target.value });
+	};
 
-let menuHome = [
-	{
-		label: "Home",
-		url:
-			"https://blackrockdigital.github.io/startbootstrap-heroic-features/#"
-	}
-];
+	Submit = s => {
+		s.preventDefault();
+		let auxdata = this.state.data;
+		this.setState({ data: auxdata.concat(this.state.value) });
+		console.log("textArray " + this.state.value);
+	};
 
-const NavBar = props => {
-	//I have to loop all the items and convert them into LIs
-
-	const itStar = props.itStar.map(item1 => (
-		<a className="navbar-brand" href={item1.url}>
-			{item1.label}
-		</a>
-	));
-
-	const itHome = props.itHome.map(item2 => (
-		<li className="nav-item active">
-			<a className="nav-link" href={item2.url}>
-				{item2.label}
-				<span className="sr-only">(current)</span>
-			</a>
-		</li>
-	));
-
-	const items = props.items.map(item => (
-		<li className="nav-item">
-			<a className="nav-link" href={item.url}>
-				{item.label}
-			</a>
-		</li>
-	));
-
-	//this methods says how the NavBar should look like in HTML
-	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-			<div className="container">
-				{itStar}
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarResponsive"
-					aria-controls="navbarResponsive"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon" />
-				</button>
-				<div className="collapse navbar-collapse" id="navbarResponsive">
-					<ul className="navbar-nav ml-auto">
-						{itHome}
-						{items}
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
-};
-
-let menujumbo = [
-	{
-		labeld: "A Warm Welcome!",
-		labelp:
-			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.",
-		labelb: "Call to action!",
-		url:
-			"https://blackrockdigital.github.io/startbootstrap-heroic-features/#"
-	}
-];
-
-const Jumbot = props => {
-	const itemsj = props.itemsj.map(itemj => (
-		<div className="container">
-			<h1 className="display-3">{itemj.labeld}</h1>
-			<p className="lead">{itemj.labelp}</p>
-			<a href={itemj.url} className="btn btn-primary btn-lg">
-				{itemj.labelb}
-			</a>
-		</div>
-	));
-
-	return <header className="jumbotron my-4">{itemsj}</header>;
-};
-
-let menucard = [
-	{
-		labelt: "Card title",
-		labelp:
-			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
-		labelf: "Find Out More!",
-		urlc: "http://placehold.it/500x325",
-		urlf:
-			"https://blackrockdigital.github.io/startbootstrap-heroic-features/#"
-	}
-];
-const Cardt = props => {
-	const itemsc = props.itemsc.map(itemc => (
-		<div className="card h-100">
-			<img className="card-img-top" src={itemc.urlc} alt="" />
-			<div className="card-body">
-				<h4 className="card-title">{itemc.labelt}</h4>
-				<p className="card-text">{itemc.labelp}</p>
-			</div>
-			<div className="card-footer">
-				<a href={itemc.urlf} className="btn btn-primary">
-					{itemc.labelf}
-				</a>
-			</div>
-		</div>
-	));
-
-	return <div className="col-lg-3 col-md-6 mb-4">{itemsc}</div>;
-};
-
-let menufoot = [
-	{
-		labelt: "Copyright © Your Website 2019"
-	}
-];
-
-const Footert = props => {
-	const itemsf = props.itemsf.map(itemf => (
-		<div className="container">
-			<p className="m-0 text-center text-white">{itemf.labelt}</p>
-		</div>
-	));
-
-	return <footer className="py-5 bg-dark">{itemsf}</footer>;
-};
-
-//create your first component
-export class Home extends React.Component {
 	render() {
 		return (
-			<div>
-				<NavBar itStar={menuStar} itHome={menuHome} items={menu} />
-				<Jumbot itemsj={menujumbo} />
-				<div className="row text-center">
-					<Cardt itemsc={menucard} />
-					<Cardt itemsc={menucard} />
-					<Cardt itemsc={menucard} />
-					<Cardt itemsc={menucard} />
+			<div className="container">
+				<nav className="navbar" align-items-center>
+					<h3>{"todos"}</h3>
+				</nav>
+				<div className="input-group mb-3">
+					<form onSubmit={this.Submit}>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="No tasks, add a task"
+							onChange={this.Change}
+							value={this.state.value}
+						/>
+					</form>
 				</div>
-				<Footert itemsf={menufoot} />
+				<br />
+				<ul className="todo-list">
+					{this.state.data.map((item, index) => (
+						<li key={index}>
+							<div className="name">
+								<span className="item-name">{item}</span>
+							</div>
+							<button className="remove">
+								<i className="material-icons">close</i>
+							</button>
+						</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
 }
+
+/*<ul className="list-group">
+
+                	<li className="list-group-item">
+						{this.state.value}
+						<button
+							type="button"
+							className="close"
+							aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</li>
+				</ul>
+
+
+
+
+*/
